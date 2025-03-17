e9cc38468659f6ce2ace5c842fe0b16b00b79796
onsiri <otapsarn@gmail.com>
2025-03-17 16:53:15 -0400

    Fix product list view and template

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import product_list
 
 urlpatterns = [
-    path("", product_list, name="product_list"),
+    path("Products/", product_list, name="product_list"),
 ]
 
 
