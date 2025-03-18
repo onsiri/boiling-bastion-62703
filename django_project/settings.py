ab6de53c017413f4eaae0bfce318cbc63267c3b1
onsiri <otapsarn@gmail.com>
2025-03-17 20:49:08 -0400

    Configure S3 for media files

--- django_project/settings.py
+++ django_project/settings.py
@@ -162,7 +162,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY+y1')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'us-east-1'  # Your bucket's region
 AWS_QUERYSTRING_AUTH = False  # Avoid auth query params for URLs
