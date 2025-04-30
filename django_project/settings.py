61573a4445fdaadcf750cbe91b100a0f3997da3a
onsiri <otapsarn@gmail.com>
2025-04-30 16:55:21 -0400

    fix ssl

--- django_project/settings.py
+++ django_project/settings.py
@@ -145,6 +145,7 @@ if not DEBUG:
 CSRF_TRUSTED_ORIGINS = [
     'https://boiling-bastion-62703-1fb7e4016adf.herokuapp.com',
     'http://localhost:8000'
+    'https://app.insightsds.com',
 ]
 
 # Custom user model
