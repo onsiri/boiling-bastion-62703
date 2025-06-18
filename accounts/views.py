88093c81085ced9551b329c76c24acf54e8041ea
onsiri <otapsarn@gmail.com>
2025-06-17 22:55:11 -0400

    update look

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from django import forms
 from .user_forms import CustomUserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, TemplateView
-
+from django.urls import reverse
 @allow_unauthenticated
 class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
@@ -16,3 +16,4 @@ class SignupPageView(generic.CreateView):
 # Add this new protected view (example)
 class HomePageView(LoginRequiredMixin, TemplateView):
     template_name = "home.html"  # Requires authentication to access
+
